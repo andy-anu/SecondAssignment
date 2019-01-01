0208afb8ebc8d3c598b1785208f81e21843c974e
andy-anu <31591034+andy-anu@users.noreply.github.com>
2019-01-01 14:06:45 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+"""
+Python Homework #2
+"""
+def artist(Name):
+    return Name
+
+def year(yearReleased):
+    return yearReleased
+
+def genre(Type):
+    return Type
+
+Artist= artist("Eminem")
+Year = year(2010)
+Genre = genre("Hip Hop")
+
+print(Artist)
+print(Year)
+print(Genre)
